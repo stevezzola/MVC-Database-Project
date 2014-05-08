2beddea195d024378cea106420c8469f01121fcb
Steven Mazzola <stevezzola@gmail.com>
2014-05-08 02:19:17 -0400

    CardLayout for switching between add panels, need to set a layout for textfields

--- view/VideoGameView.java
+++ view/VideoGameView.java
@@ -1,10 +1,12 @@
 package view;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -39,6 +41,21 @@ public class VideoGameView extends javax.swing.JFrame {
 	private JRadioButton jRadioButton2;
 	private JRadioButton jRadioButton1;
 	private JLabel jLabel3;
+	private JLabel jLabel4;
+	private JLabel jLabel5;
+	private JTextField jTextField7;
+	private JLabel jLabel8;
+	private JTextField jTextField6;
+	private JPanel jPanel6;
+	private JComboBox<String> jComboBox1;
+	private JComboBox<String> jComboBox2;
+	private JComboBox<String> jComboBox3;
+	private JLabel jLabel9;
+	private JLabel jLabel7;
+	private JTextField jTextField5;
+	private JLabel jLabel6;
+	private JTextField jTextField4;
+	private JTextField jTextField3;
 	private JPanel jPanel5;
 	private ButtonGroup buttonGroup1;
 	public JTextArea jTextArea1;
@@ -51,6 +68,16 @@ public class VideoGameView extends javax.swing.JFrame {
 	private JPanel jPanel4;
 	private ActionListener radioListener1;
 	private ActionListener radioListener2;
+	private JLabel jLabela;
+	private JLabel jLabelb;
+	private JLabel jLabelc;
+	private JLabel jLabeld;
+	private JLabel jLabele;
+	private JTextField jTextFielda;
+	private JTextField jTextFieldb;
+	private JTextField jTextFielde;
+	private JTextField jTextFieldd;
+	private JPanel cards;
 	
 	public VideoGameView() { 
 		super();
@@ -78,8 +105,127 @@ public class VideoGameView extends javax.swing.JFrame {
 						jPanel4.setPreferredSize(new java.awt.Dimension(180, 56));
 						
 							jPanel5 = new JPanel();
-							jPanel3.add(jPanel5, BorderLayout.CENTER);
-						
+							//jPanel3.add(jPanel5, BorderLayout.CENTER);
+							{
+								jLabel4 = new JLabel();
+								jPanel5.add(jLabel4);
+								jLabel4.setText("Name:");
+							}
+							{
+								jTextField3 = new JTextField();
+								jPanel5.add(jTextField3);
+								jTextField3.setPreferredSize(new java.awt.Dimension(119, 23));
+							}
+							{
+								jLabel5 = new JLabel();
+								jPanel5.add(jLabel5);
+								jLabel5.setText("ID #");
+							}
+							{
+								jTextField4 = new JTextField();
+								jPanel5.add(jTextField4);
+								jTextField4.setPreferredSize(new java.awt.Dimension(117, 23));
+							}
+							{
+								jLabel6 = new JLabel();
+								jPanel5.add(jLabel6);
+								jLabel6.setText("Gender: ");
+							}
+							{
+								String[] genders = { "Male", "Female"};
+								jComboBox1 = new JComboBox<String>(genders);
+								jPanel5.add(jComboBox1);
+								jComboBox1.setPreferredSize(new java.awt.Dimension(77, 23));
+							}
+							{
+								jLabel7 = new JLabel();
+								jPanel5.add(jLabel7);
+								jLabel7.setText("Age:");
+								jLabel7.setPreferredSize(new java.awt.Dimension(48, 16));
+							}
+							{
+								jTextField6 = new JTextField();
+								jPanel5.add(jTextField6);
+								jTextField6.setPreferredSize(new java.awt.Dimension(117, 23));
+							}
+							{
+								jLabel8 = new JLabel();
+								jPanel5.add(jLabel8);
+								jLabel8.setText("Birth Date:");
+							}
+							{
+								jTextField7 = new JTextField();
+								jPanel5.add(jTextField7);
+								jTextField7.setPreferredSize(new java.awt.Dimension(89, 23));
+							}
+							{
+								jLabel9 = new JLabel();
+								jPanel5.add(jLabel9);
+								jLabel9.setText("Play Level:");
+							}
+							{
+								String[] playLevels = { "Beginner", "Casual", "Intermediate", "Advanced", "Hardcore" };
+								jComboBox2 = new JComboBox<String>(playLevels);
+								jPanel5.add(jComboBox2);
+							}
+					
+							jPanel6 = new JPanel();
+							//jPanel3.add(jPanel6, BorderLayout.CENTER);
+							{
+								jLabela = new JLabel();
+								jPanel6.add(jLabela);
+								jLabela.setText("Title:");
+							}
+							{
+								jTextFielda = new JTextField();
+								jPanel6.add(jTextFielda);
+								jTextFielda.setPreferredSize(new java.awt.Dimension(119, 23));
+							}
+							{
+								jLabelb = new JLabel();
+								jPanel6.add(jLabelb);
+								jLabelb.setText("ID #");
+							}
+							{
+								jTextFieldb = new JTextField();
+								jPanel6.add(jTextFieldb);
+								jTextFieldb.setPreferredSize(new java.awt.Dimension(117, 23));
+							}
+							{
+								jLabelc = new JLabel();
+								jPanel6.add(jLabelc);
+								jLabelc.setText("Console: ");
+							}
+							{
+								String[] consoles = {"PC", "Xbox One", "PS4", "Wii U"};
+								jComboBox3 = new JComboBox<String>(consoles);
+								jPanel6.add(jComboBox3);
+							}
+							{
+								jLabeld = new JLabel();
+								jPanel6.add(jLabeld);
+								jLabeld.setText("Company:");
+							}
+							{
+								jTextFieldd = new JTextField();
+								jPanel6.add(jTextFieldd);
+								jTextFieldd.setPreferredSize(new java.awt.Dimension(87, 23));
+							}
+							{
+								jLabele = new JLabel();
+								jPanel6.add(jLabele);
+								jLabele.setText("Price:");
+							}
+							{
+								jTextFielde = new JTextField();
+								jPanel6.add(jTextFielde);
+								jTextFielde.setPreferredSize(new java.awt.Dimension(117, 23));
+							}
+							
+							cards = new JPanel(new CardLayout());
+							cards.add(jPanel5, "Customer");
+							cards.add(jPanel6, "Game");
+							jPanel3.add(cards, BorderLayout.CENTER);
 
 							jLabel3 = new JLabel();
 							jPanel4.add(jLabel3);
@@ -101,13 +247,13 @@ public class VideoGameView extends javax.swing.JFrame {
 							
 							radioListener1 = new ActionListener() {
 								public void actionPerformed(ActionEvent evt1) {
-									System.out.println("1");
+									changeForm(1);
 								}
 							};
 							
 							radioListener2 = new ActionListener() {
 								public void actionPerformed(ActionEvent evt1) {
-									System.out.println("2");
+									changeForm(2);
 								}
 							};
 							
@@ -170,6 +316,16 @@ public class VideoGameView extends javax.swing.JFrame {
 			input = jTextField2.getText();
 			GameController controller = new GameController(new Game(), this);
 			controller.read(input);
+		}
+	}
+	
+	private void changeForm(int index) {
+		CardLayout cl = (CardLayout)(cards.getLayout());
+	   	if (index == 1) {
+	   		cl.show(cards, "Customer");
+		}
+		else if (index == 2) {
+			cl.show(cards, "Game");
 		}
 	}
 }
